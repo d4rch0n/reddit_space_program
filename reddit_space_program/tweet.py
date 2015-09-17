e763a9487b49ece51f68682b86b28735b5794d70
Johan Nestaas <johannestaas@gmail.com>
2015-09-17 01:01:02 -0700

    tweet via cli

--- reddit_space_program/tweet.py
+++ reddit_space_program/tweet.py
@@ -9,13 +9,16 @@ from reddit_space_program import config
 def connect():
     return twitter.Api(**config.twitter)
 
+def tweet(conn, msg):
+    conn.PostUpdate(msg)
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('tweet')
     args = parser.parse_args()
     conn = connect()
-    conn.PostUpdate(args.tweet)
+    tweet(conn, args.tweet)
 
 if __name__ == '__main__':
     main()
