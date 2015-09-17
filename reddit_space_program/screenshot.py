2bbad271d41d70761f70b8e468f7086ebe4beb72
Johan Nestaas <johannestaas@gmail.com>
2015-09-17 12:38:40 -0700

    take screenshot

--- reddit_space_program/screenshot.py
+++ reddit_space_program/screenshot.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
+from subprocess import call
+import shlex
+from uuid import uuid1
+
 from imgurpython import ImgurClient
+
 from reddit_space_program import config
+
+screenshot_dir = config.screenshot_dir or os.path.expanduser('~/kerbal_screenshots')
+if not os.path.exists(screenshot_dir):
+    os.makedirs(screenshot_dir)
 
 def connect():
     return ImgurClient(**config.imgur)
+
+def screenshot(window):
+    path = os.path.join(screenshot_dir, uuid1() + '.jpg')
+    call(shlex.split('import -window {} {}'.format(window, path)))
 
 def upload(conn, path):
     return conn.upload_from_path(path, anon=False)['link']
@@ -12,10 +25,11 @@ def upload(conn, path):
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('path')
+    parser.add_argument('--window', '-w', default=config.window)
     args = parser.parse_args()
     conn = connect()
-    link = upload(conn, args.path)
+    path = screenshot(args.window)
+    link = upload(conn, path)
     print(link)
 
 if __name__ == '__main__':
