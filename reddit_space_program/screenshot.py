b3ec5a2637be29281690a215f0ea41cdc8480774
Johan Nestaas <johannestaas@gmail.com>
2015-09-18 18:15:50 -0700

    add recordmydesktop video recording

--- reddit_space_program/screenshot.py
+++ reddit_space_program/screenshot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from subprocess import call
+import time
+from subprocess import call, Popen, PIPE, CalledProcessError
 import shlex
 from uuid import uuid1
 
@@ -22,6 +23,35 @@ def screenshot(window=config.window):
     path = os.path.join(screenshot_dir, str(uuid1()) + '.jpg')
     call(shlex.split('import -window {} {}'.format(window, path)))
     return path
+
+def record(window=config.window, output=None, cap_time=5):
+    if output is None:
+        output = str(uuid1())
+    prog1 = Popen(shlex.split(
+        'recordmydesktop -o {} --windowid {} --no-sound'.format(
+            output, window
+        )), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    time.sleep(cap_time)
+    os.kill(prog1.pid, 2)
+    while True:
+        try:
+            os.kill(prog1.pid, 0)
+        except OSError:
+            break
+        else:
+            prog1.communicate('\n')
+            time.sleep(0.1)
+    inp_fname = output + '.ogv'
+    out_path = os.path.join(screenshot_dir, output + '.mp4')
+    prog2 = Popen(shlex.split(
+        'avconv -i {} {}'.format(
+            inp_fname,
+            out_path,
+        )), stdout=PIPE, stderr=PIPE)
+    while not os.path.exists(out_path):
+        time.sleep(0.1)
+    os.remove(inp_fname)
+    return out_path
 
 def upload(conn, path):
     if TEST_PHASE:
